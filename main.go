b7ff44e291a84d3cee5c39bd3895ed8501c9c586
motoki-shun <motoki-shun@kayac.com>
2018-09-29 22:56:02 +0900

    cli base

--- main.go
+++ main.go
@@ -2,10 +2,50 @@ package main
 
 import (
     "os"
+    "fmt"
     "github.com/urfave/cli"
 )
 
 func main() {
   app := cli.NewApp()
+
+  app.Name = "eb-ssh"
+  app.Usage = "This app echo input arguments"
+  app.Version = "0.0.1"
+
+  app.Action = func (context *cli.Context) error {
+    if context.Bool("env-name") {
+      fmt.Println("env-name: " + context.Args().Get(0))
+    } else if context.Bool("region") {
+      fmt.Println("region: " + context.Args().Get(0))
+    } else {
+      fmt.Println(context.Args().Get(0))
+    }
+    return nil
+  }
+
+  app.Flags = []cli.Flag{
+    cli.StringFlag {
+      Name: "env-name, e",
+      Usage: "",
+    },
+    cli.StringFlag {
+      Name: "region, r",
+      Usage: "",
+    },
+    cli.StringFlag {
+      Name: "profile, p",
+      Usage: "",
+    },
+    cli.StringFlag {
+      Name: "keyfile, k",
+      Usage: "",
+    },
+    cli.StringFlag {
+      Name: "proxy-host, P",
+      Usage: "",
+    },
+  }
+
   app.Run(os.Args)
 }
